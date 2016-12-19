668d831dce9fa2ce3d1e3665d1bafd24e9daf15d
razvanolar <rolar.xiss@gmail.com>
2016-12-19 19:55:51 +0200

    Use ClustersAlg outside the project.

--- src/sample/utils/clustering/ClusteringAlg.java
+++ src/sample/utils/clustering/ClusteringAlg.java
@@ -2,7 +2,10 @@ package sample.utils.clustering;
 
 import sample.models.Cluster;
 import sample.models.ClusterEntry;
+import sample.utils.repository.FileRepository;
+import sample.utils.repository.Repository;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,13 +16,27 @@ public class ClusteringAlg {
 
   public static void main(String[] args) {
     try {
-      int clusterNumber = 500;
-      float optimization = (float) 0.2;
-
+      System.out.println("Validating arguments");
       ClusteringAlg clusteringAlg = new ClusteringAlg();
-
-//      List<Cluster> clusters = clusteringAlg.kmean(rawEntries, clusterNumber, optimization);
-//      clusteringAlg.printClusters(clusters);
+      if (!clusteringAlg.validateParameters(args)) {
+        clusteringAlg.printHelpMessage();
+        return;
+      }
+      System.out.println("Initialising repository");
+      System.out.println(ClusteringAlg.class.getProtectionDomain().getCodeSource().getLocation().getPath());
+      Repository repo = new FileRepository(args[1], ClusteringAlg.class.getProtectionDomain().getCodeSource().getLocation().getPath() + ".\\sample\\utils\\data\\attribute_range.txt");
+      boolean[] mask = {true, true, true, true, true, true, true, true};
+      System.out.println("Retrieve entries");
+      List<ClusterEntry> clusterEntries = repo.getPartialRawEntries(mask);
+      int clustersNumber = Integer.parseInt(args[0]);
+      if (clusterEntries == null || clustersNumber > clusterEntries.size()) {
+        System.out.println("Unable to compute clusters on so few entries.");
+        clusteringAlg.printHelpMessage();
+        return;
+      }
+      System.out.println("Run clustering...");
+      List<Cluster> clusters = clusteringAlg.kmean(clusterEntries, clustersNumber, (float) 0.2);
+      clusteringAlg.printClusters(clusters);
     } catch (Exception e) {
       e.printStackTrace();
     }
@@ -73,10 +90,38 @@ public class ClusteringAlg {
     return clusters;
   }
 
-  public void printClusters(List<Cluster> clusters) {
+  private boolean validateParameters(String[] args) {
+    if (args == null || args.length != 2) {
+      System.out.println("Incorrect provided parameters.");
+      return false;
+    }
+    try {
+      if (Integer.parseInt(args[0]) < 2) {
+        System.out.println("Incorrect provided clusters number.");
+        return false;
+      }
+      File file = new File(args[1]);
+      if (!file.exists()) {
+        System.out.println("Specified entries file does not exists.");
+        return false;
+      }
+      return true;
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+    return false;
+  }
+
+  private void printHelpMessage() {
+    System.out.println("java ClusteringAlg clusters_number entries_path");
+    System.out.println("\tclusters_number - number of clusters to be generated");
+    System.out.println("\tentries_path - path to the file containing the entries on which to apply the clustering");
+  }
+
+  private void printClusters(List<Cluster> clusters) {
     for (Cluster c : clusters) {
       for (ClusterEntry p : c.getClusterEntries()) {
-        System.out.println("Cluster: " + clusters.indexOf(c) + "\t RawEntry: " + p);
+        System.out.println("Cluster: " + clusters.indexOf(c) + "\t RawEntry: " + p.getValues());
       }
     }
   }
